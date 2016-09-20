983637311ec60408e3b7026cf35587827819aa5f
Mikhail Korobov <kmike84@gmail.com>
2016-09-21 00:59:15 +0500

    scipy is now also required

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'numpy >= 1.9.0',
+        'scipy',
         'singledispatch >= 3.4.0.3',
         'six',
     ],
