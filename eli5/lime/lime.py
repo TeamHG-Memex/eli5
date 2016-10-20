85767cf4209ea5b01b5fbed332a43bcb270af2f1
Mikhail Korobov <kmike84@gmail.com>
2016-10-20 15:24:33 +0200

    fix scikit-learn warning

--- eli5/lime/lime.py
+++ eli5/lime/lime.py
@@ -56,8 +56,11 @@ from typing import Any, Callable
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
 from sklearn.pipeline import make_pipeline
+try:
+    from sklearn.model_selection import train_test_split
+except ImportError:  # sklearn < 0.18
+    from sklearn.cross_validation import train_test_split
 
 from eli5.lime import textutils
 from eli5.lime.samplers import BaseSampler, MaskingTextSampler
