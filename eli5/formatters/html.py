c64744b8344f35e317c6f81941f8a8e0d6100bb8
Mikhail Korobov <kmike84@gmail.com>
2016-10-24 20:46:49 +0200

    allow to customize display order for html formatter

--- eli5/formatters/html.py
+++ eli5/formatters/html.py
@@ -35,10 +35,6 @@ def format_as_html(explanation, include_styles=True, force_weights=True,
     in the document.
     """
     template = template_env.get_template('explain.html')
-    explanation = copy.deepcopy(explanation)
-    for field in fields.ALL:
-        if field not in show:
-            explanation.pop(field, None)
 
     return template.render(
         include_styles=include_styles,
@@ -48,6 +44,7 @@ def format_as_html(explanation, include_styles=True, force_weights=True,
         td1_styles='padding: 0 1em 0 0.5em; text-align: right; border: none;',
         tdm_styles='padding: 0 0.5em 0 0.5em; text-align: center; border: none;',
         td2_styles='padding: 0 0.5em 0 0.5em; text-align: left; border: none;',
+        show=show,
         **explanation)
 
 
