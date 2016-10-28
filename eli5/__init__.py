a598aa62f98a2f19e209652e7dc5d27c587ce3b0
Mikhail Korobov <kmike84@gmail.com>
2016-10-29 01:31:32 +0500

    lightning support

--- eli5/__init__.py
+++ eli5/__init__.py
@@ -6,3 +6,11 @@ __version__ = '0.0.6'
 from .formatters import format_as_html, format_html_styles, format_as_text
 from .explain import explain_weights, explain_prediction
 from .sklearn import explain_weights_sklearn, explain_prediction_sklearn
+try:
+    from .lightning import (
+        explain_prediction_lightning,
+        explain_weights_lightning
+    )
+except ImportError as e:
+    # lightning is not available
+    pass
