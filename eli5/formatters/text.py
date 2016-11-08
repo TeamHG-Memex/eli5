aaa1fb1c347866ff74c64f8c7ad18b5d5a14e85c
Mikhail Korobov <kmike84@gmail.com>
2016-11-08 23:16:37 +0500

    PY2 fixed Python 2 compatibility

--- eli5/formatters/text.py
+++ eli5/formatters/text.py
@@ -70,7 +70,7 @@ def _error_lines(explanation):
 def _feature_importances_lines(explanation, hl_spaces):
     sz = _maxlen(explanation.feature_importances)
     for name, w, std in explanation.feature_importances:
-        yield '{w:0.4f} {plus} {std:0.4f} {feature}'.format(
+        yield u'{w:0.4f} {plus} {std:0.4f} {feature}'.format(
             feature=_format_feature(name, hl_spaces).ljust(sz),
             w=w,
             plus=_PLUS_MINUS,
@@ -145,7 +145,7 @@ def _max_feature_size(explanation):
 
 def _format_feature_weights(feature_weights, sz, hl_spaces):
     return [
-        '{weight:+8.3f}  {feature}'.format(
+        u'{weight:+8.3f}  {feature}'.format(
             weight=coef,
             feature=_format_feature(name, hl_spaces=hl_spaces).ljust(sz))
         for name, coef in feature_weights]
@@ -160,6 +160,8 @@ def _format_remaining(remaining, kind):
 
 
 def _format_feature(name, hl_spaces):
+    if isinstance(name, bytes):
+        name = name.decode('utf8')
     if isinstance(name, FormattedFeatureName):
         return name.format()
     elif isinstance(name, list) and \
