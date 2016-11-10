84ebba9c7d9290a7c1b72e82c7ba99a1f3b0db9f
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-11-10 20:08:43 +0300

    Fix return values of placeholder functions
    This allows to get nicer type completions in the IDE.

--- eli5/explain.py
+++ eli5/explain.py
@@ -5,20 +5,22 @@ are defined in submodules.
 """
 from singledispatch import singledispatch
 
+from eli5.base import Explanation
+
 
 @singledispatch
 def explain_prediction(estimator, doc, **kwargs):
-    """ Return an explanation of an estimator """
-    return {
-        "estimator": repr(estimator),
-        "description": "Error: estimator %r is not supported" % estimator,
-    }
+    """ Return an explanation of an estimator prediction """
+    return Explanation(
+        estimator=repr(estimator),
+        error="estimator %r is not supported" % estimator,
+    )
 
 
 @singledispatch
 def explain_weights(estimator, **kwargs):
-    """ Return an explanation of an estimator """
-    return {
-        "estimator": repr(estimator),
-        "description": "Error: estimator %r is not supported" % estimator,
-    }
+    """ Return an explanation of an estimator weights """
+    return Explanation(
+        estimator=repr(estimator),
+        error="estimator %r is not supported" % estimator,
+    )
