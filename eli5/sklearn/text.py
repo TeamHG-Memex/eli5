d2782e9a9ebe0d8ce910e066bc43d0bd5c93874f
Mikhail Korobov <kmike84@gmail.com>
2016-12-14 04:32:23 +0500

    this variable doesn’t have to be dict

--- eli5/sklearn/text.py
+++ eli5/sklearn/text.py
@@ -121,7 +121,7 @@ def _get_other(feature_weights, named_found_features):
     # search for items that were not accounted at all.
     other_items = []
     accounted_keys = set()  # type: Set[Tuple[str, int]]
-    all_found_features = {}
+    all_found_features = set()
     for _, found_features in named_found_features:
         all_found_features.update(found_features)
 
