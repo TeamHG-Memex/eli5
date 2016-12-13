handle a case when .fit haven’t seen all labels and thus predict_proba has incorrect shape

eli5/lime/utils.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from typing import List, Any
 
 import numpy as np
 from scipy.stats import entropy
-
 from sklearn.pipeline import Pipeline
 from sklearn.utils import check_random_state
 from sklearn.utils.metaestimators import if_delegate_has_method
@@ -48,6 +48,25 @@ def fit_proba(clf, X, y_proba, expand_factor=10, sample_weight=None,
     fit_params.setdefault(param_name, sample_weight)
     clf.fit(X, y, **fit_params)
     return clf
+
+
+def fix_multiclass_predict_proba(y_proba, seen_classes, complete_classes):
+    # type: (np.ndarray, List[Any], List[Any]) -> np.ndarray
+    """
+    Add missing columns to predict_proba result.
+
+    When a multiclass classifier is fit on a dataset which onlly contains
+    a subset of possible classes its predict_proba result only has columns
+    corresponding to seen classes. This function adds missing columns.
+    """
+    assert set(complete_classes) >= set(seen_classes)
+    y_proba_fixed = np.zeros(
+        shape=(y_proba.shape[0], len(complete_classes)),
+        dtype=y_proba.dtype,
+    )
+    class_mapping = np.searchsorted(complete_classes, seen_classes)
+    y_proba_fixed[:, class_mapping] = y_proba
+    return y_proba_fixed
 
 
 class _PipelinePatched(Pipeline):

tests/test_lime_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_absolute_error
 
-from eli5.lime.utils import fit_proba
+from eli5.lime.utils import fit_proba, fix_multiclass_predict_proba
 
 
 def test_fit_proba():
@@ -59,3 +59,20 @@ def test_fit_proba():
               random_state=42)
     y_pred4 = clf4.predict_proba(X)[:,1]
     assert np.allclose(y_pred, y_pred4)
+
+
+def test_fix_multiclass_predict_proba():
+    y_proba = np.array(
+        [[0.0, 1.0], [0.0, 1.0], [1.0, 0.0], [0.5, 0.5], [0.5, 0.5]],
+        dtype=np.float32
+    )
+    y_proba_fixed = fix_multiclass_predict_proba(y_proba, seen_classes=[0, 2],
+                                                 complete_classes=[0, 1, 2, 3])
+    assert y_proba_fixed.dtype == np.float32
+    assert np.array_equal(y_proba_fixed, np.array([
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [1.0, 0.0, 0.0, 0.0],
+        [0.5, 0.0, 0.5, 0.0],
+        [0.5, 0.0, 0.5, 0.0],
+    ]))