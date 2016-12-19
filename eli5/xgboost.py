91c230f8104bbb92b5a918a18c06fa63c1b801cd
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-12-19 14:47:48 +0300

    Parsing xgboost dump

--- eli5/xgboost.py
+++ eli5/xgboost.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import re
 from singledispatch import singledispatch
+from typing import List, Dict
 
 import numpy as np
 from xgboost import XGBClassifier, XGBRegressor
@@ -48,3 +50,50 @@ def explain_weights_xgboost(xgb,
         estimator=repr(xgb),
         method='feature importances',
     )
+
+
+def parse_dump(text_dump):
+    result = None
+    stack = []  # type: List[Dict]
+    for line in text_dump.split('\n'):
+        if line:
+            depth, node = _parse_dump_line(line)
+            if depth == 0:
+                assert not stack
+                result = node
+                stack.append(node)
+            elif depth > len(stack):
+                raise ValueError('Unexpected dump structure')
+            else:
+                if depth < len(stack):
+                    stack = stack[:depth]
+                stack[-1].setdefault('children', []).append(node)
+                stack.append(node)
+    return result
+
+
+def _parse_dump_line(line):
+    branch_match = re.match(
+        '^(\t*)(\d+):\[(\w+)<([^\]]+)\] yes=(\d+),no=(\d+),missing=(\d+)$', line)
+    if branch_match:
+        tabs, node_id, feature, condition, yes, no, missing = \
+            branch_match.groups()
+        depth = len(tabs)
+        return depth, {
+            'depth': depth,
+            'nodeid': int(node_id),
+            'split': feature,
+            'split_condition': float(condition),
+            'yes': int(yes),
+            'no': int(no),
+            'missing': int(missing),
+        }
+    leaf_match = re.match('^(\t*)(\d+):leaf=(.*)$', line)
+    if leaf_match:
+        tabs, node_id, value = leaf_match.groups()
+        depth = len(tabs)
+        return depth, {
+            'nodeid': int(node_id),
+            'leaf': float(value),
+        }
+    raise ValueError('Line in unexpected format: {}'.format(line))
--- tests/test_xgboost.py
+++ tests/test_xgboost.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import pytest
+from sklearn.datasets import make_classification
 pytest.importorskip('xgboost')
 from xgboost import XGBClassifier, XGBRegressor
 
+from eli5.xgboost import parse_dump
 from .test_sklearn_explain_weights import (
     test_explain_random_forest as _check_rf,
     test_explain_random_forest_and_tree_feature_re as _check_rf_feature_re,
@@ -23,3 +25,53 @@ def test_explain_xgboost_feature_re(newsgroups_train):
 
 def test_feature_importances_no_remaining():
     _check_rf_no_remaining(XGBClassifier())
+
+
+def test_parse_dump():
+    text_dump = '''\
+0:[f2<-0.391121] yes=1,no=2,missing=1
+	1:[f3<-1.15681] yes=3,no=4,missing=3
+		3:leaf=0.0545455
+		4:leaf=0.180952
+	2:[f3<1.7511] yes=5,no=6,missing=5
+		5:[f2<-0.0136578] yes=7,no=8,missing=7
+			7:leaf=-0.0545455
+			8:leaf=-0.182979
+		6:leaf=0.111111
+'''
+    assert parse_dump(text_dump) == {
+        'children': [
+            {'children': [{'leaf': 0.0545455, 'nodeid': 3},
+                          {'leaf': 0.180952, 'nodeid': 4}],
+             'depth': 1,
+             'missing': 3,
+             'no': 4,
+             'nodeid': 1,
+             'split': 'f3',
+             'split_condition': -1.15681,
+             'yes': 3},
+            {'children': [
+                {'children': [{'leaf': -0.0545455, 'nodeid': 7},
+                              {'leaf': -0.182979, 'nodeid': 8}],
+                 'depth': 2,
+                 'missing': 7,
+                 'no': 8,
+                 'nodeid': 5,
+                 'split': 'f2',
+                 'split_condition': -0.0136578,
+                 'yes': 7},
+                {'leaf': 0.111111, 'nodeid': 6}],
+             'depth': 1,
+             'missing': 5,
+             'no': 6,
+             'nodeid': 2,
+             'split': 'f3',
+             'split_condition': 1.7511,
+             'yes': 5}],
+        'depth': 0,
+        'missing': 1,
+        'no': 2,
+        'nodeid': 0,
+        'split': 'f2',
+        'split_condition': -0.391121,
+        'yes': 1}
