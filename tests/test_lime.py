f1b0905a28595189535d336214806a3ced1ae4ba
Mikhail Korobov <kmike84@gmail.com>
2016-12-28 03:28:51 +0500

    TST check TextExplainer(position_dependent=True)

--- tests/test_lime.py
+++ tests/test_lime.py
@@ -40,13 +40,12 @@ def test_lime_flat_neighbourhood(newsgroups_train):
     docs, y, target_names = newsgroups_train
     doc = docs[0]
 
-    def predict_proba(docs):
+    @_apply_to_list
+    def predict_proba(doc):
         """ This function predicts non-zero probabilities only for 3 labels """
         proba_graphics = [0, 1.0, 0, 0]
         proba_other = [0.9, 0, 0.1, 0]
-        return np.array(
-            [proba_graphics if 'file' in doc else proba_other for doc in docs]
-        )
+        return proba_graphics if 'file' in doc else proba_other
 
     te = TextExplainer(expand_factor=None, random_state=42)
     te.fit(doc, predict_proba)
@@ -65,11 +64,10 @@ def test_lime_flat_neighbourhood(newsgroups_train):
                          [[None], ['.']])
 def test_text_explainer_char_based(token_pattern):
     text = "Hello, world!"
-    def predict_proba(docs):
-        return np.array([
-            [0, 1] if 'lo' in doc else [1, 0]
-            for doc in docs
-        ], dtype=np.float)
+
+    @_apply_to_list
+    def predict_proba(doc):
+        return [0.0, 1.0] if 'lo' in doc else [1.0, 0.0]
 
     te = TextExplainer(char_based=True, token_pattern=token_pattern)
     te.fit(text, predict_proba)
@@ -81,3 +79,37 @@ def test_text_explainer_char_based(token_pattern):
     format_as_all(res, te.clf_)
     check_targets_scores(res)
     assert res.targets[0].feature_weights.pos[0].feature == 'lo'
+
+
+def test_text_explainer_position_dependent():
+    text = "foo bar baz egg spam foo bar baz egg spam"
+
+    @_apply_to_list
+    def predict_proba(doc):
+        tokens = doc.split()
+        # 'bar' is only important in the beginning of the document,
+        # not in the end
+        return [0, 1] if len(tokens) >= 2 and tokens[1] == 'bar' else [1, 0]
+
+    # bag of words model is not powerful enough to explain predict_proba above
+    te = TextExplainer(random_state=42)
+    te.fit(text, predict_proba)
+    print(te.metrics_)
+    assert te.metrics_['score'] < 0.9
+    assert te.metrics_['mean_KL_divergence'] > 0.3
+
+    # position_dependent=True can make it work
+    te = TextExplainer(position_dependent=True, random_state=42)
+    te.fit(text, predict_proba)
+    print(te.metrics_)
+    assert te.metrics_['score'] > 0.95
+    assert te.metrics_['mean_KL_divergence'] < 0.3
+
+    expl = te.explain_prediction()
+    format_as_all(expl, te.clf_)
+
+
+def _apply_to_list(func):
+    def wrapper(docs):
+        return np.array([func(doc) for doc in docs])
+    return wrapper
