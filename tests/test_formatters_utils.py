28e6a0bf5328989fa2e2b0273e6108ff4023356f
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-12-30 15:19:37 +0300

    Add tabulate and format_value tests

---
+++ tests/test_formatters_utils.py
@@ -0,0 +1,54 @@
+import pytest
+
+from eli5.formatters.utils import tabulate, format_value
+
+
+def test_tabulate():
+    assert tabulate([]) == []
+    assert tabulate([], header=['foo', 'bar']) == [
+        'foo  bar',
+        '---  ---',
+    ]
+    assert tabulate([[1, 'oneee'], [2.3, 'two']]) == [
+        '1    oneee',
+        '2.3  two  ',
+    ]
+    assert tabulate([[1, 'oneee'], [2.3, 'two']], header=['Digit', '']) == [
+        'Digit       ',
+        '-----  -----',
+        '1      oneee',
+        '2.3    two  ',
+    ]
+    assert tabulate([[1, 'oneee'], [2.3, 'two']], header=['Digit', 'Word'],
+                    col_align='lr') == [
+        'Digit   Word',
+        '-----  -----',
+        '1      oneee',
+        '2.3      two',
+    ]
+    assert tabulate([[1, 'oneee'], [2.3, 'two']], header=['Digit', 'Word'],
+                    col_align='r') == [
+        'Digit   Word',
+        '-----  -----',
+        '    1  oneee',
+        '  2.3    two',
+    ]
+    assert tabulate([[1, 'oneee'], [2.3, 'two']], header=['Digit', 'Word'],
+                    col_align=['c', 'r']) == [
+        'Digit   Word',
+        '-----  -----',
+        '  1    oneee',
+        ' 2.3     two',
+    ]
+    with pytest.raises(ValueError):
+        assert tabulate([[1, 'oneee'], [2.3]])
+    with pytest.raises(ValueError):
+        assert tabulate([[1], [2.3]], header=['Digit', 'Word'])
+    with pytest.raises(ValueError):
+        assert tabulate([[1], [2.3]], header=['Digit'], col_align='rr')
+
+
+def test_format_value():
+    assert format_value(None) == ''
+    assert format_value(float('nan')) == 'Missing'
+    assert format_value(12.23333334) == '+12.233'
