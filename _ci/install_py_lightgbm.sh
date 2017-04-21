17906813c859feefa75eafd246d9fbb33ef011ea
Mikhail Korobov <kmike84@gmail.com>
2017-04-21 18:46:54 +0500

    TST trying to enable lightgbm in tox & travis

---
+++ _ci/install_py_lightgbm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ -z "${LIGHTGBM_CHECKOUT}" ]; then
+    echo "LIGHTGBM_CHECKOUT is not set; LightGBM is not installed"
+    exit 1
+fi
+
+pushd "${LIGHTGBM_CHECKOUT}/python-package/"
+/usr/bin/env python setup.py install
+popd
