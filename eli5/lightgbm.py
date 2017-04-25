081e6d590f95e8b418b82f6a02bfce7ae3434748
Mikhail Korobov <kmike84@gmail.com>
2017-04-25 05:38:26 +0500

    initial version of explain_prediction_lightgbm

--- eli5/lightgbm.py
+++ eli5/lightgbm.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from singledispatch import singledispatch
+from collections import defaultdict
+from typing import DefaultDict
 
+import numpy as np  # type: ignore
 import lightgbm  # type: ignore
-from eli5.explain import explain_weights
+
+from eli5._feature_weights import get_top_features
+from eli5.base import Explanation, TargetExplanation
+from eli5.explain import explain_weights, explain_prediction
 from eli5._feature_importances import get_feature_importance_explanation
+from eli5.sklearn.text import add_weighted_spans
+from eli5.sklearn.utils import handle_vec, get_X, add_intercept, predict_proba
+from eli5.utils import get_target_display_names, mask
 
 
 DESCRIPTION_LIGHTGBM = """
@@ -15,7 +23,6 @@ all values sum to 1.
 
 @explain_weights.register(lightgbm.LGBMClassifier)
 @explain_weights.register(lightgbm.LGBMRegressor)
-@singledispatch
 def explain_weights_lightgbm(lgb,
                              vec=None,
                              top=20,
@@ -59,6 +66,107 @@ def explain_weights_lightgbm(lgb,
     )
 
 
+@explain_prediction.register(lightgbm.LGBMClassifier)
+@explain_prediction.register(lightgbm.LGBMRegressor)
+def explain_prediction_lightgbm(
+        lgb, doc,
+        vec=None,
+        top=None,
+        top_targets=None,
+        target_names=None,
+        targets=None,
+        feature_names=None,
+        feature_re=None,
+        feature_filter=None,
+        vectorized=False,
+        ):
+    """ Return an explanation of LightGBM prediction (via scikit-learn wrapper
+    LGBMClassifier or LGBMRegressor) as feature weights.
+
+    See :func:`eli5.explain_prediction` for description of
+    ``top``, ``top_targets``, ``target_names``, ``targets``,
+    ``feature_names``, ``feature_re`` and ``feature_filter`` parameters.
+
+    ``vec`` is a vectorizer instance used to transform
+    raw features to the input of the estimator ``xgb``
+    (e.g. a fitted CountVectorizer instance); you can pass it
+    instead of ``feature_names``.
+
+    ``vectorized`` is a flag which tells eli5 if ``doc`` should be
+    passed through ``vec`` or not. By default it is False, meaning that
+    if ``vec`` is not None, ``vec.transform([doc])`` is passed to the
+    estimator. Set it to False if you're passing ``vec``,
+    but ``doc`` is already vectorized.
+
+    Method for determining feature importances follows an idea from
+    http://blog.datadive.net/interpreting-random-forests/.
+    Feature weights are calculated by following decision paths in trees
+    of an ensemble.
+    Each leaf has an output score, and expected scores can also be assigned
+    to parent nodes.
+    Contribution of one feature on the decision path is how much expected score
+    changes from parent to child.
+    Weights of all features sum to the output score of the estimator.
+    """
+
+    vec, feature_names = handle_vec(lgb, doc, vec, vectorized, feature_names)
+    if feature_names.bias_name is None:
+        # LightGBM estimators do not have an intercept, but here we interpret
+        # them as having an intercept
+        feature_names.bias_name = '<BIAS>'
+    X = get_X(doc, vec, vectorized=vectorized)
+    is_regression = isinstance(lgb, lightgbm.LGBMRegressor)
+
+    proba = predict_proba(lgb, X)
+
+    weights_dict = _get_prediction_feature_weights(lgb, X[0])
+    weights = _target_feature_weights(weights_dict, feature_names)
+    score = _get_score(weights_dict)
+    x, = add_intercept(X)
+    # x = _missing_values_set_to_nan(x, xgb.missing, sparse_missing=True)
+
+    feature_names, flt_indices = feature_names.handle_filter(
+        feature_filter, feature_re, x)
+
+    # is_multiclass = _xgb_n_targets(xgb) > 1
+    names = lgb.classes_ if not is_regression else ['y']
+    display_names = get_target_display_names(names, target_names, targets,
+                                             top_targets, proba)
+
+    def get_feature_weights(_label_id):
+        assert _label_id == 0
+        _feature_weights = weights
+        _x = x
+        if flt_indices is not None:
+            _x = mask(_x, flt_indices)
+            _feature_weights = mask(_feature_weights, flt_indices)
+        return get_top_features(feature_names, _feature_weights, top, _x)
+
+    res = Explanation(
+        estimator=repr(lgb),
+        method='decision paths',
+        description='',
+        # description={
+        #     (False, False): DESCRIPTION_CLF_BINARY,
+        #     (False, True): DESCRIPTION_CLF_MULTICLASS,
+        #     (True, False): DESCRIPTION_REGRESSION,
+        # }[is_regression, is_multiclass],
+        is_regression=is_regression,
+        targets=[],
+    )
+
+    feature_weights = get_feature_weights(0)
+    target_expl = TargetExplanation(
+        target=display_names[-1][1],
+        feature_weights=feature_weights,
+        score=score,
+        proba=proba[1] if proba is not None else None,
+    )
+    add_weighted_spans(doc, vec, vectorized, target_expl)
+    res.targets.append(target_expl)
+    return res
+
+
 def _get_lgb_feature_importances(lgb, importance_type):
     aliases = {'weight': 'split'}
     coef = lgb.booster_.feature_importance(
@@ -66,3 +174,100 @@ def _get_lgb_feature_importances(lgb, importance_type):
     )
     norm = coef.sum()
     return coef / norm if norm else coef
+
+
+def _compute_node_values(tree_info):
+    """ Add node_value key with an average value for non-leaf nodes """
+    def walk(tree):
+        if 'leaf_value' in tree:
+            return tree['leaf_value'], tree['leaf_count']
+        left_value, left_count = walk(tree['left_child'])
+        right_value, right_count = walk(tree['right_child'])
+        count = left_count + right_count
+        tree['node_value'] = (left_value * left_count +
+                              right_value * right_count) / count
+        return tree['node_value'], count
+
+    for tree in tree_info:
+        walk(tree['tree_structure'])
+
+
+def _get_decision_path(leaf_index, split_index, leaf_id):
+    path, split_features = [], []
+    parent_id, leaf = leaf_index[leaf_id]
+    path.append(leaf['leaf_value'])
+    while True:
+        parent_id, node = split_index[parent_id]
+        path.append(node['node_value'])
+        split_features.append(node['split_feature'])
+        if parent_id is None:
+            break
+
+    path.reverse()
+    changes = _changes(path)
+    bias, path = changes[0], list(zip(reversed(split_features), changes[1:]))
+    return bias, path
+
+
+def _changes(path):
+    """
+    >>> _changes([2, 3, 0, 5])
+    [2, 1, -3, 5]
+    >>> _changes([2])
+    [2]
+    """
+    res = [path[0]]
+    res += [p - p_prev for p, p_prev in zip(path[1:], path)]
+    return res
+
+
+def _get_leaf_split_indices(tree_structure):
+    leaf_index = {}   # leaf id => (parent_id, leaf)
+    split_index = {}  # split id => (parent_id, subtree)
+
+    def walk(tree, parent_id=None):
+        if 'leaf_index' in tree:
+            leaf_index[tree['leaf_index']] = tree['leaf_parent'], tree
+        else:
+            split_index[tree['split_index']] = parent_id, tree
+            walk(tree['left_child'], tree['split_index'])
+            walk(tree['right_child'], tree['split_index'])
+
+    walk(tree_structure)
+    return leaf_index, split_index
+
+
+def _get_prediction_feature_weights(lgb, x):
+    """ 
+    Return {feat_id: value} dict with feature weights, following ideas from 
+    http://blog.datadive.net/interpreting-random-forests/  
+    """
+    dump = lgb.booster_.dump_model()
+    tree_info = dump['tree_info']
+    _compute_node_values(tree_info)
+    pred_leafs = lgb.booster_.predict([x], pred_leaf=True).reshape(-1)
+
+    feature_weights = defaultdict(float)  # type: DefaultDict[str, float]
+    for info, leaf_id in zip(tree_info, pred_leafs):
+        leaf_index, split_index = _get_leaf_split_indices(
+            info['tree_structure']
+        )
+        bias, path = _get_decision_path(leaf_index, split_index, leaf_id)
+        feature_weights[None] += bias
+        for feat, value in path:
+            feature_weights[feat] += value
+    return dict(feature_weights)
+
+
+def _target_feature_weights(feature_weights_dict, feature_names):
+    feature_weights = np.zeros(len(feature_names))
+    for k, v in feature_weights_dict.items():
+        if k is None:
+            feature_weights[feature_names.bias_idx] = v
+        else:
+            feature_weights[k] = v
+    return feature_weights
+
+
+def _get_score(feature_weights_dict):
+    return sum(feature_weights_dict.values())
