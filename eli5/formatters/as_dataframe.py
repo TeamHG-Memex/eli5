a951506cb3162a0a437f1bc8183cf718e6b88844
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2017-06-05 10:51:27 +0300

    dataframe export: dispatch and format_as_dataframes

--- eli5/formatters/as_dataframe.py
+++ eli5/formatters/as_dataframe.py
@@ -1,26 +1,52 @@
 from itertools import chain
-from typing import List, Optional
+from singledispatch import singledispatch
+from typing import Dict, List, Optional
+import warnings
 
-import pandas as pd
+import pandas as pd  # type: ignore
 
 from eli5.base import (
     Explanation, FeatureImportances, TargetExplanation, TransitionFeatureWeights,
 )
 
 
+EXPORTED_ATTRIBUTES = ['transition_features', 'targets', 'features_importances']
+
+
+def format_as_dataframes(expl):
+    # type: (Explanation) -> Dict[str, pd.DataFrame]
+    """ Export an explanation to a dictionary with pandas.DataFrame values
+    and string keys that correspond to explanation attributes.
+    Use this method if several dataframes can be exported from a single explanation.
+    """
+    result = {}
+    for attr in EXPORTED_ATTRIBUTES:
+        value = getattr(expl, attr)
+        if value:
+            result[attr] = format_as_dataframe(value)
+    return result
+
+
+@singledispatch
 def format_as_dataframe(expl):
     # type: (Explanation) -> Optional[pd.DataFrame]
-    """ Export an explanation to pandas.DataFrame. Only target weights and
-    feature importances can be exported, else None is returned.
+    """ Export an explanation to a single pandas.DataFrame.
+    In case several dataframes could be exported by format_as_dataframes,
+    a warning is raised. If no dataframe can be exported, None is returned.
     """
-    if expl.transition_features:
-        return transition_features_to_df(expl.transition_features)
-    elif expl.targets:
-        return targets_to_df(expl.targets)
-    elif expl.feature_importances:
-        return feature_importances_to_df(expl.feature_importances)
+    for attr in EXPORTED_ATTRIBUTES:
+        value = getattr(expl, attr)
+        if value:
+            other_attrs = [a for a in EXPORTED_ATTRIBUTES
+                           if getattr(expl, a) and a != attr]
+            if other_attrs:
+                warnings.warn('Exporting {} to DataFrame, but also {} could be '
+                              'exported. Consider using eli5.format_as_dataframes.'
+                              .format(attr, ', '.join(other_attrs)))
+            return format_as_dataframe(value)
 
 
+@format_as_dataframe.register(FeatureImportances)
 def feature_importances_to_df(feature_importances):
     # type: (FeatureImportances) -> pd.DataFrame
     weights = feature_importances.importances
@@ -33,8 +59,11 @@ def feature_importances_to_df(feature_importances):
     return df
 
 
+@format_as_dataframe.register(list)
 def targets_to_df(targets):
     # type: (List[TargetExplanation]) -> pd.DataFrame
+    if targets and not isinstance(targets[0], TargetExplanation):
+        raise ValueError('Only lists of TargetExplanation are supported')
     index, weights, stds, values = [], [], [], []
     for target in targets:
         for fw in chain(target.feature_weights.pos,
@@ -53,6 +82,7 @@ def targets_to_df(targets):
     return df
 
 
+@format_as_dataframe.register(TransitionFeatureWeights)
 def transition_features_to_df(transition_features):
     # type: (TransitionFeatureWeights) -> pd.DataFrame
     class_names = transition_features.class_names
