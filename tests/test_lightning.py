c3738c2764182a07d7a8c1f48aaede58f2a53ec6
Mikhail Korobov <kmike84@gmail.com>
2017-06-28 23:13:08 +0500

    TST cleanup lightning tests

--- tests/test_lightning.py
+++ tests/test_lightning.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 import pytest
 pytest.importorskip('lightning')
 
-from lightning import regression
+from lightning import regression, classification
 from lightning.impl.base import BaseEstimator
 from sklearn.multiclass import OneVsRestClassifier
 
@@ -24,8 +24,17 @@ from .test_sklearn_explain_weights import (
     assert_explained_weights_linear_regressor,
 )
 
+_DEFAULTS = {
+    classification.AdaGradClassifier: dict(alpha=1e-9, random_state=42),
+    classification.SDCAClassifier: dict(alpha=1e-9, random_state=42),
+    regression.SGDRegressor: dict(max_iter=10, alpha=1e5, random_state=42)
+}
 
-@pytest.mark.parametrize(['clf'], [[clf()] for clf in _CLASSIFIERS])
+def _instances(classes):
+    return [[cls(**_DEFAULTS.get(cls, {}))] for cls in classes]
+
+
+@pytest.mark.parametrize(['clf'], _instances(_CLASSIFIERS))
 def test_explain_predition_classifiers(newsgroups_train, clf):
     clf = OneVsRestClassifier(clf)
     assert_multiclass_linear_classifier_explained(newsgroups_train, clf,
@@ -35,13 +44,13 @@ def test_explain_predition_classifiers(newsgroups_train, clf):
                                                       explain_prediction_lightning)
 
 
-@pytest.mark.parametrize(['clf'], [[clf()] for clf in _CLASSIFIERS])
+@pytest.mark.parametrize(['clf'], _instances(_CLASSIFIERS))
 def test_explain_predition_classifiers_binary(newsgroups_train_binary, clf):
     assert_binary_linear_classifier_explained(newsgroups_train_binary, clf,
                                               explain_prediction)
 
 
-@pytest.mark.parametrize(['clf'], [[clf()] for clf in _CLASSIFIERS])
+@pytest.mark.parametrize(['clf'], _instances(_CLASSIFIERS))
 def test_explain_weights_classifiers(newsgroups_train, clf):
     clf = OneVsRestClassifier(clf)
     assert_explained_weights_linear_classifier(newsgroups_train, clf,
@@ -52,25 +61,19 @@ def test_explain_weights_classifiers(newsgroups_train, clf):
             explain_weights=explain_weights_lightning)
 
 
-regressors = [r for r in _REGRESSORS if r is not regression.SGDRegressor]
-regressor_params = (
-    [[reg()] for reg in regressors] +
-    [[regression.SGDRegressor(max_iter=10, alpha=1e5, random_state=42)]]
-)
-
-@pytest.mark.parametrize(['reg'], regressor_params)
+@pytest.mark.parametrize(['reg'], _instances(_REGRESSORS))
 def test_explain_prediction_regressors(boston_train, reg):
     assert_linear_regression_explained(boston_train, reg, explain_prediction,
                                        atol=1e-3)
 
 
-@pytest.mark.parametrize(['reg'], regressor_params)
+@pytest.mark.parametrize(['reg'], _instances(_REGRESSORS))
 def test_explain_weights_regressors(boston_train, reg):
     assert_explained_weights_linear_regressor(boston_train, reg,
                                               has_bias=False)
 
 
-@pytest.mark.parametrize(['reg'], regressor_params[:2])
+@pytest.mark.parametrize(['reg'], _instances(_REGRESSORS)[:2])
 def test_explain_prediction_pandas(reg, boston_train):
     _check_explain_prediction_pandas(reg, boston_train)
 
