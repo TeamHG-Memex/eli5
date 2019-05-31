eed04ef877bc1704262596e4f2eebde64c2f9472
teabolt <tomasbaltrunas0@gmail.com>
2019-05-31 12:21:17 +0100

    Make blended heatmap transitions smoother

--- eli5/formatters/image.py
+++ eli5/formatters/image.py
@@ -32,21 +32,31 @@ def format_as_image(expl):
     image = np.float32(image / 255)
 
     # apply colour map
-    heatmap_grayscale = heatmap[:,:,0]
+    heatmap_grayscale = heatmap[:,:,0] # save grayscale width*height version
     heatmap = np.float32(matplotlib.cm.jet(heatmap[:,:,0]))
 
     # insert alpha channel
     image = np.dstack((image, np.ones(image_dimensions, dtype=np.float32)))
     # print(heatmap.shape, heatmap.dtype, np.min(heatmap), np.max(heatmap))
 
-    # threshold
+    # modify alpha
+    # 1.
     # heatmap = np.where(heatmap < 0.85, 0*heatmap, heatmap) # too dark
     # heatmap = np.where(heatmap < 0.85, image, heatmap) # too bright
-    threshold = 0.1
-    heatmap_alpha = heatmap[:,:,3]
-    heatmap_alpha[heatmap_grayscale < threshold] = 0.0
-    heatmap_alpha[threshold < heatmap_grayscale] = 0.5
-    heatmap[:,:,3] = heatmap_alpha
+    # 2.
+    # heatmap_alpha = heatmap[:,:,3]
+    # threshold = 0.1
+    # heatmap_alpha[heatmap_grayscale < threshold] = 0.0
+    # heatmap_alpha[threshold < heatmap_grayscale] = 0.5
+    # heatmap[:,:,3] = heatmap_alpha
+    # 3. FIXES: make the transition from no heatmap to heatmap less sharp
+    # alpha intensity corresponds / is proportional to grayscale value intensity
+    heatmap_alpha = heatmap_grayscale
+    # cap the intensity so that it's not too bright when near 1
+    cap = 0.65
+    heatmap_alpha = np.minimum(heatmap_alpha, cap) # choose the lower of two values
+    heatmap[:,:,3] = heatmap_alpha # update alpha channel
+    # TODO: efficiency of this approach?
 
     fig, ax = plt.subplots()
     ax.axis('off')
@@ -59,6 +69,7 @@ def format_as_image(expl):
     # H = ax.contourf(heatmap[:,:,0], alpha=0.5, clim=[100, 255])
 
     plt.show()
+    # FIXME: for some reason image is resized to 231x231 from 299x299
 
     # overlayed_image = np.float32(heatmap) + np.float32(image)
     # overlayed_image = 255 * overlayed_image / np.max(overlayed_image)
