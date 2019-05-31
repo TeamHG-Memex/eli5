e27380664ea49c6a796d3de8bf484459fd6b7cf5
teabolt <tomasbaltrunas0@gmail.com>
2019-05-31 13:20:31 +0100

    Add ability to pass target prediction to do Grad-CAM on

--- eli5/keras.py
+++ eli5/keras.py
@@ -14,21 +14,25 @@ from eli5.explain import explain_prediction
 
 @explain_prediction.register(Model)
 def explain_prediction_keras(estimator, doc, # model, image
-                             top=None, # not supported
-                             top_targets=None, # not supported
-                             target_names=None, # rename prediction labels
+                             top=None, # NOT SUPPORTED
+                             top_targets=None, # NOT SUPPORTED
+                             target_names=None, # rename / supply prediction labels
                              targets=None, # prediction(s) to focus on, if None take top prediction
-                             feature_names=None, # not supported
-                             feature_re=None, # not supported
-                             feature_filter=None, # not supported
+                             feature_names=None, # NOT SUPPORTED
+                             feature_re=None, # NOT SUPPORTED
+                             feature_filter=None, # NOT SUPPORTED
                              # new parameters:
                              layers=None, # which layer(s) to focus on,
-                             preprocessing=None, # function 
+                             preprocessing=None, # preprocessing function 
+                             target_decoder=None, # prediction decoding function
                             ):
     """Explain prediction of a Keras model
     doc : image, 
         one of: path to a single image, directory containing images, 
         PIL image object, or an array can also be multiple images.
+    targets: predictions
+        a list of predictions
+        integer for ImageNet classification
     layers: target layer to Grad-CAM on,
         one of: a layer (valid keras layer name (str) or index (int)), a list of layers
     """
@@ -46,12 +50,38 @@ def explain_prediction_keras(estimator, doc, # model, image
     # TODO: grad-cam on multiple layers by passing a list to layers
     layer_name, layer_index = get_target_layer(layers)
 
-    heatmap = jacobgil(model=estimator, preprocessed_input=image, layer=(layer_name, layer_index))
+    # get prediction to focus on
+    if targets is None:
+        predicted = get_target_prediction(estimator, image, decoder=target_decoder)
+    else:
+        predicted = targets[0] 
+        # TODO: take in a single target as well, not just a list
+        # does it make sense to take a list of targets. You can only Grad-CAM a single target?
+        # TODO: consider changing signature / types for explain_prediction generic function
+        # TODO: need to find a way to show the label for the passed prediction as well as its probability
+
+    heatmap = jacobgil(estimator, image, predicted, layer=(layer_name, layer_index))
     heatmap = array_to_img(heatmap)
     image = array_to_img(image[0])
     explanation.heatmap = heatmap
     explanation.image = image
     return explanation
+
+
+def get_target_prediction(model, preprocessed_input, decoder=None):
+    predictions = model.predict(preprocessed_input)
+    if decoder is not None:
+        # TODO: check if decoder is callable?
+        # FIXME: it is not certain that we need such indexing into the decoder's output
+        top_1 = decoder(predictions)[0][0] 
+        ncode, label, proba = top_1
+        # TODO: do I print, log, or append to 'description' this output?
+        print('Predicted class:') 
+        print('%s (%s) with probability %.2f' % (label, ncode, proba))
+    # FIXME: non-classification tasks
+    predicted_class = np.argmax(predictions)
+    return predicted_class
+
 
 def get_target_layer(layers):
     layer_name = None
@@ -82,7 +112,7 @@ def load_image(img_path, xDims=None, preprocess_fn=None):
     # x = next(ImageDataGenerator(rescale=1.0/255).flow(x))
     return x
 
-def jacobgil(model=None, preprocessed_input=None, layer=(None, None)):
+def jacobgil(model, preprocessed_input, predicted_class, layer=(None, None)):
     from keras.applications.vgg16 import (
     VGG16, decode_predictions)
     from keras.models import Model
@@ -94,7 +124,6 @@ def jacobgil(model=None, preprocessed_input=None, layer=(None, None)):
     import tensorflow as tf
     import keras
     import sys
-    import cv2
 
     def target_category_loss(x, category_index, nb_classes):
         return tf.multiply(x, K.one_hot([category_index], nb_classes))
@@ -110,7 +139,9 @@ def jacobgil(model=None, preprocessed_input=None, layer=(None, None)):
         return [grad if grad is not None else tf.zeros_like(var) for var, grad in zip(var_list, grads)]
 
     def grad_cam(input_model, image, category_index, layer_name=None, layer_index=None):
-        nb_classes = 1000
+        # FIXME: this assumes that we are doing classification
+        nb_classes = 1000 # FIXME: number of classes can be variable
+    
         target_layer = lambda x: target_category_loss(x, category_index, nb_classes)
         x = Lambda(target_layer, output_shape = target_category_loss_output_shape)(input_model.output)
         model = Model(inputs=input_model.input, outputs=x)
@@ -122,7 +153,7 @@ def jacobgil(model=None, preprocessed_input=None, layer=(None, None)):
         # conv_output = conv_output[0].output
 
         # TODO: automatically get last Conv layer if layer_name and layer_index are None
-        # need to check BOTH name and index
+        # be sure to check BOTH name and index is None
         layer_index = -4 if (layer_name is None and layer_index is None) else layer_index # FIXME: don't hardcode four
         # we need to get the output attribute, else we get a TypeError: Failed to convert object to tensor
         # bottom-up horizontal graph traversal
@@ -130,7 +161,7 @@ def jacobgil(model=None, preprocessed_input=None, layer=(None, None)):
         grads = normalize(_compute_gradients(loss, [conv_output])[0])
         gradient_function = K.function([model.input], [conv_output, grads])
 
-        output, grads_val = gradient_function([image])
+        output, grads_val = gradient_function([image]) # work happens here
         output, grads_val = output[0, :], grads_val[0, :, :, :] # FIXME: this probably assumes that the layer is a width*height filter
 
         weights = np.mean(grads_val, axis = (0, 1))
@@ -152,12 +183,6 @@ def jacobgil(model=None, preprocessed_input=None, layer=(None, None)):
 
     # model = VGG16(weights='imagenet')
 
-    predictions = model.predict(preprocessed_input)
-    top_1 = decode_predictions(predictions)[0][0]
-    print('Predicted class:')
-    print('%s (%s) with probability %.2f' % (top_1[1], top_1[0], top_1[2]))
-
-    predicted_class = np.argmax(predictions)
     heatmap = grad_cam(model, preprocessed_input, predicted_class, layer_name=layer[0], layer_index=layer[1])
     # cv2.imwrite("gradcam.jpg", cam)
     return heatmap
