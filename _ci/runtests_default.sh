0f65f92d731856216286c9e2dbc74429a9ac8983
teabolt <tomasbaltrunas0@gmail.com>
2019-06-27 11:29:40 +0100

    Update _ci with keras package

--- _ci/runtests_default.sh
+++ _ci/runtests_default.sh
@@ -4,8 +4,7 @@ py.test --doctest-modules \
         --ignore eli5/xgboost.py \
         --ignore eli5/lightgbm.py \
         --ignore eli5/catboost.py \
-        --ignore eli5/keras.py \
+        --ignore eli5/keras \
         --ignore eli5/formatters/image.py \
-        --ignore eli5/ipython_image.py \
         --ignore tests/utils_image.py \
         --cov=eli5 --cov-report=html --cov-report=term  "$@"
