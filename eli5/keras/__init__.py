d37016cdfa4b0cc782ec38677e1677789327eac3
teabolt <tomasbaltrunas0@gmail.com>
2019-07-08 14:55:24 +0100

    Add compute_weights to keras package

--- eli5/keras/__init__.py
+++ eli5/keras/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from .explain_prediction import explain_prediction_keras
-from .gradcam import gradcam, gradcam_backend
+from .gradcam import gradcam, gradcam_backend, compute_weights
