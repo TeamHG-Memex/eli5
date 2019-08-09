54b120b7c0cb7fee07df90e10eeb4cd8ab90f376
teabolt <tomasbaltrunas0@gmail.com>
2019-08-09 12:24:42 +0100

    Ignore keras estimator .py files in no deps tests (CI fix)

--- _ci/runtests_default.sh
+++ _ci/runtests_default.sh
@@ -7,6 +7,6 @@ py.test --doctest-modules \
         --ignore eli5/keras \
         --ignore eli5/formatters/image.py \
         --ignore tests/utils_image.py \
-        --ignore tests/estimators/keras_sentiment_classifier.py \
-        --ignore tests/estimators/keras_multiclass_text_classifier.py \
+        --ignore tests/estimators/keras_sentiment_classifier/keras_sentiment_classifier.py \
+        --ignore tests/estimators/keras_multiclass_text_classifier/keras_multiclass_text_classifier.py \
         --cov=eli5 --cov-report=html --cov-report=term  "$@"
