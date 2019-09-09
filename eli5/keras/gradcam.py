18980b72ee41f773c20069b2b4aa9ccedbfb7d7e
teabolt <tomasbaltrunas0@gmail.com>
2019-09-09 22:09:57 +0200

    Separate out classification-specific code into a function

--- eli5/keras/gradcam.py
+++ eli5/keras/gradcam.py
@@ -56,26 +56,13 @@ def gradcam_backend_keras(model, # type: Model
         (numpy.ndarray, np.ndarray, np.ndarray, np.ndarray)
         Values of variables.
     """
-    # score for class in targets
-    # TODO: maybe do the sum / loss calculation in this function and pass it to gradcam.
-    # This would be consistent with what is done in
-    # https://github.com/ramprs/grad-cam/blob/master/misc/utils.lua
-    # and https://github.com/ramprs/grad-cam/blob/master/classification.lua
-    # TODO: as in pytorch PR, separate out classification tensor code
-    if targets is not None:
-        _validate_targets(targets)
-        _validate_classification_target(targets[0], model.output_shape)
-        target, = targets
-        predicted_idx = K.constant([target], dtype='int64')
-    else:
-        predicted_idx = _autoget_target_prediction(model)
-    # access value by index
-    predicted_val = K.gather(model.output[0, :], predicted_idx)
+    # class score
+    predicted_idx, predicted_val = _classification_target(model, targets)
 
     # output of target activation layer, i.e. activation maps of a conv layer
     activation_output = activation_layer.output
 
-    # score for class w.r.p.t. activation layer
+    # score w.r.p.t. activation layer
     grads = _calc_gradient(predicted_val, [activation_output])
 
     # TODO: gradcam on input layer
@@ -97,7 +84,7 @@ def _calc_gradient(ys, xs):
     """
     Return the gradient of scalar ``ys`` with respect to each of list ``xs``,
     (must be singleton)
-    and apply grad normalization.
+    and apply gradient normalization.
     """
     # differentiate ys (scalar) with respect to each variable in xs
     # K.gradients tends to produce bigger values than tf.gradients
@@ -123,9 +110,24 @@ def _calc_gradient(ys, xs):
     return grads
 
 
-def _autoget_target_prediction(model):
-    # type: (Model) -> K.variable
-    """Automatically get the index with 
-    the highest predicted output from ``model``"""
-    output = model.output
-    return K.argmax(output, axis=-1)
+def _classification_target(model, targets):
+    # type: (Model, Optional[List[int]]) -> Tuple[K.variable, K.variable]
+    """Get a predicted index and its value from a classification based model."""
+    # TODO: maybe pass the loss/score to the gradcam function.
+    # This would be consistent with what is done in
+    # https://github.com/ramprs/grad-cam/blob/master/misc/utils.lua
+    # and https://github.com/ramprs/grad-cam/blob/master/classification.lua
+    if targets is not None:
+        _validate_targets(targets)
+        target, = targets
+        _validate_classification_target(target, model.output_shape)
+        # make a dummy index
+        predicted_idx = K.constant([target], dtype='int64')
+    else:
+        # take the index with the highest value
+        # from the array of predictions
+        predicted_idx = K.argmax(model.output, axis=-1)
+
+    # access value by index
+    predicted_val = K.gather(model.output[0, :], predicted_idx)
+    return predicted_idx, predicted_val
